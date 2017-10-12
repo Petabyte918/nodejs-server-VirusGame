01e02ffc528e55455d45ef11caed467797ad0a17
lucaskhane <dani.kn17.rjch@gmail.com>
2017-10-12 17:26:25 +0200

    First real test with heroku

---
+++ server.js
@@ -0,0 +1,34 @@
+// Dependencies
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+var port = process.env.PORT || 5000;
+//app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname+"/static/", 'index.html'));
+});
+
+// Starts the server.
+server.listen(port, function() {
+//server.listen(5000, function() {
+  console.log('Starting server');
+});
+
+
+
+
+var players = {};
+io.on('connection', function(socket) {
+	socket.on('new player', function() {
+	    console.log("Server. New player");
+	});
+});
